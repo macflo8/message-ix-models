74811bfeb0a122670a79b80d3ef5f209093c9478
Florian Maczek <macflo@gmx.at>
2024-09-02 17:43:08 +0200

    Extend type hints and docstrings to data_util

--- message_ix_models/model/material/data_util.py
+++ message_ix_models/model/material/data_util.py
@@ -25,6 +25,8 @@ from message_ix_models.util import (
 )
 
 if TYPE_CHECKING:
+    from message_ix import Scenario
+
     from message_ix_models import Context
 
 
@@ -1643,7 +1645,7 @@ def add_coal_lowerbound_2020(sc):
     )
 
 
-def add_cement_bounds_2020(sc, temp):
+def add_cement_bounds_2020(sc, temp: str = "high_temp"):
     """Set lower and upper bounds for gas and oil as a calibration for 2020"""
 
     final_resid = pd.read_csv(
@@ -1893,7 +1895,7 @@ def add_cement_bounds_2020(sc, temp):
 
 
 def read_sector_data(
-    scenario: message_ix.Scenario, sectname: str, ssp: str
+    scenario: message_ix.Scenario, sectname: str, ssp: str, filename: str
 ) -> pd.DataFrame:
     """
     Read sector data for industry with sectname
@@ -2044,7 +2046,7 @@ def read_timeseries(
 
 def read_rel(
     scenario: message_ix.Scenario, material: str, ssp: str or None, filename: str
-):
+) -> pd.DataFrame:
     """
     Read relation_* type parameter data for specific industry
 
@@ -2134,7 +2136,9 @@ def gen_te_projections(
     return inv_cost, fix_cost
 
 
-def get_ssp_soc_eco_data(context: "Context", model: str, measure: str, tec):
+def get_ssp_soc_eco_data(
+    context: "Context", model: str, measure: str, tec
+) -> pd.DataFrame:
     """
     Function to update scenario GDP and POP timeseries to SSP 3.0
     and format to MESSAGEix "bound_activity_*" DataFrame
@@ -2226,7 +2230,6 @@ def calculate_ini_new_cap(df_demand, technology, material):
     return make_df("initial_new_capacity_up", **df_demand)
 
 
-
 def get_thermal_industry_emi_coefficients(scen: message_ix.Scenario) -> pd.DataFrame:
     """
     Pulls existing parametrization for non-CO2 emission
@@ -2274,20 +2277,22 @@ def get_thermal_industry_emi_coefficients(scen: message_ix.Scenario) -> pd.DataF
     return df_joined
 
 
-def get_furnace_inputs(scen: message_ix.Scenario, first_year) -> pd.DataFrame:
-    """
-    Pulls existing parametrization for input coefficients
-     of given Scenario instance and returns only for technologies
-     with "furnace" in the name
+def get_furnace_inputs(scen: message_ix.Scenario, first_year: int) -> pd.DataFrame:
+    """Return existing parametrization for input coefficients of given Scenario instance
+     and returns only for technologies with "furnace" in the name
 
     Parameters
     ----------
     scen: message_ix.Scenario
         Scenario instance to pull input parameter from
+    first_year: int
+        Earliest year for which furnace input parameter should be retrieved
 
     Returns
     -------
     pd.DataFrame
+        a dataframe of furnace input paramter with index
+        ["node_loc", "year_act", "commodity", "technology"]
     """
     furn_tecs = "furnace"
     df_furn = scen.par("input")
@@ -2344,7 +2349,7 @@ def calculate_furnace_non_co2_emi_coeff(
     return df_final_new
 
 
-def calibrate_t_d_tecs(scenario):
+def calibrate_t_d_tecs(scenario: "Scenario"):
     # ------------------------------------------------------
     # Revise t_d historical_activity and 2020 activity bounds
     # ------------------------------------------------------
@@ -2402,7 +2407,8 @@ def calibrate_t_d_tecs(scenario):
             .set_index(["node_loc", "technology", "year_act"])
         )
 
-        # Multiply the activity data with the input efficiencies to derive total energy requirements
+        # Multiply the activity data with the input efficiencies to derive total energy
+        # requirements
         act["eff"] = (
             inp.loc[
                 (inp.year_vtg == inp.year_act)
@@ -2450,7 +2456,14 @@ def calibrate_t_d_tecs(scenario):
         scenario.add_par("bound_activity_up", update_df)
 
 
-def maybe_add_water_tecs(scenario):
+def maybe_add_water_tecs(scenario: "Scenario") -> None:
+    """Adds water supply technologies that are required for the Materials build
+
+    Parameters
+    ----------
+    scenario: .Scenario
+        instance to check for water technologies and add if missing
+    """
     if "water_supply" not in list(scenario.set("level")):
         scenario.check_out()
         # add missing water tecs
@@ -2467,7 +2480,16 @@ def maybe_add_water_tecs(scenario):
         scenario.commit("add missing water tecs")
 
 
-def calibrate_for_SSPs(scenario):
+def calibrate_for_SSPs(scenario: "Scenario") -> None:
+    """Adjust technologies activity bounds and growth constraints to avoid base year
+    infeasibilities in year 2020. Specifically developed for the SSP_dev scenarios,
+    where most technology activities are fixed in 2020.
+
+    Parameters
+    ----------
+    scenario: .Scenario
+        instance to apply parameter changes to
+    """
     add_elec_i_ini_act(scenario)
 
     # prohibit electric clinker kilns in first decade
