ef763cb3b9e085cedbca078ea1f604de1a0e3720
Florian Maczek <macflo@gmx.at>
2025-05-05 10:55:12 +0200

    Deactivate max global steel recycling relation

--- message_ix_models/model/material/data_steel.py
+++ message_ix_models/model/material/data_steel.py
@@ -483,69 +483,69 @@ def gen_data_steel(scenario: message_ix.Scenario, dry_run: bool = False):
             data_steel, results, params, t, yv_ya, nodes, global_region
         )
 
-    # Add relation for the maximum global scrap use in 2020
-    for t in [
-        "scrap_recovery_steel_1",
-        "scrap_recovery_steel_2",
-        "scrap_recovery_steel_3",
-    ]:
-        df_max_recycling = pd.DataFrame(
-            {
-                "relation": "max_global_recycling_steel",
-                "node_rel": "R12_GLB",
-                "year_rel": 2020,
-                "year_act": 2020,
-                "node_loc": nodes,
-                "technology": t,
-                "mode": "M1",
-                "unit": "???",
-                "value": data_steel_rel.loc[
-                    (
-                        (data_steel_rel["relation"] == "max_global_recycling_steel")
-                        & (data_steel_rel["parameter"] == "relation_activity")
-                    ),
-                    "value",
-                ].values[0],
-            }
-        )
-
-        results["relation_activity"].append(df_max_recycling)
-
-    df_max_recycling_upper = pd.DataFrame(
-        {
-            "relation": "max_global_recycling_steel",
-            "node_rel": "R12_GLB",
-            "year_rel": 2020,
-            "unit": "???",
-            "value": data_steel_rel.loc[
-                (
-                    (data_steel_rel["relation"] == "max_global_recycling_steel")
-                    & (data_steel_rel["parameter"] == "relation_upper")
-                ),
-                "value",
-            ].values[0],
-        },
-        index=[0],
-    )
-    df_max_recycling_lower = pd.DataFrame(
-        {
-            "relation": "max_global_recycling_steel",
-            "node_rel": "R12_GLB",
-            "year_rel": 2020,
-            "unit": "???",
-            "value": data_steel_rel.loc[
-                (
-                    (data_steel_rel["relation"] == "max_global_recycling_steel")
-                    & (data_steel_rel["parameter"] == "relation_lower")
-                ),
-                "value",
-            ].values[0],
-        },
-        index=[0],
-    )
-    results["relation_activity"].append(df_max_recycling)
-    results["relation_upper"].append(df_max_recycling_upper)
-    results["relation_lower"].append(df_max_recycling_lower)
+    # # Add relation for the maximum global scrap use in 2020
+    # for t in [
+    #     "scrap_recovery_steel_1",
+    #     "scrap_recovery_steel_2",
+    #     "scrap_recovery_steel_3",
+    # ]:
+    #     df_max_recycling = pd.DataFrame(
+    #         {
+    #             "relation": "max_global_recycling_steel",
+    #             "node_rel": "R12_GLB",
+    #             "year_rel": 2020,
+    #             "year_act": 2020,
+    #             "node_loc": nodes,
+    #             "technology": t,
+    #             "mode": "M1",
+    #             "unit": "???",
+    #             "value": data_steel_rel.loc[
+    #                 (
+    #                     (data_steel_rel["relation"] == "max_global_recycling_steel")
+    #                     & (data_steel_rel["parameter"] == "relation_activity")
+    #                 ),
+    #                 "value",
+    #             ].values[0],
+    #         }
+    #     )
+    #
+    #     results["relation_activity"].append(df_max_recycling)
+    #
+    # df_max_recycling_upper = pd.DataFrame(
+    #     {
+    #         "relation": "max_global_recycling_steel",
+    #         "node_rel": "R12_GLB",
+    #         "year_rel": 2020,
+    #         "unit": "???",
+    #         "value": data_steel_rel.loc[
+    #             (
+    #                 (data_steel_rel["relation"] == "max_global_recycling_steel")
+    #                 & (data_steel_rel["parameter"] == "relation_upper")
+    #             ),
+    #             "value",
+    #         ].values[0],
+    #     },
+    #     index=[0],
+    # )
+    # df_max_recycling_lower = pd.DataFrame(
+    #     {
+    #         "relation": "max_global_recycling_steel",
+    #         "node_rel": "R12_GLB",
+    #         "year_rel": 2020,
+    #         "unit": "???",
+    #         "value": data_steel_rel.loc[
+    #             (
+    #                 (data_steel_rel["relation"] == "max_global_recycling_steel")
+    #                 & (data_steel_rel["parameter"] == "relation_lower")
+    #             ),
+    #             "value",
+    #         ].values[0],
+    #     },
+    #     index=[0],
+    # )
+    # results["relation_activity"].append(df_max_recycling)
+    # results["relation_upper"].append(df_max_recycling_upper)
+    # results["relation_lower"].append(df_max_recycling_lower)
     # Add relations for scrap grades and availability
     regions = set(data_steel_rel["Region"].values)
     gen_data_steel_rel(data_steel_rel, results, regions, modelyears)
@@ -629,7 +629,7 @@ def gen_data_steel(scenario: message_ix.Scenario, dry_run: bool = False):
     )
 
     df_fin_use_r12 = get_consumption_ts("SSP2")
-    scrap_avail = get_weibull_scrap_release(df_fin_use_r12)
+    scrap_avail = get_weibull_scrap_release(df_fin_use_r12, recovery_factor=0.95)
     demand = gen_demand("SSP2")
 
     results = combine_df_dictionaries(
